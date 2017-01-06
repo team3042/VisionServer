7b358a27b52ee04f01ef871840c9c2591b690ac9
FRCTeam3042 <team3042@gmail.com>
2017-01-05 20:06:42 -0600

    Roborio side vision server

---
+++ src/phoneConnection/TestServer.java
@@ -0,0 +1,59 @@
+package phoneConnection;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.SocketException;
+
+import org.spectrum3847.RIOdroid.RIOadb;
+
+public class TestServer{
+	private static int port = 3042;
+	private ServerSocket socket;
+	public TestServer(){
+		RIOadb.init();      //Start up ADB deamon and get an instance of jadb
+		System.out.println(RIOadb.getDevicesList());
+		int numOfDevice = RIOadb.getDevicesList().size();
+		if(numOfDevice>0){
+			System.out.println("Going ahead with port forwarding...");
+		    AdbCmds.adbForward(port, port); //Forward our port to the phones port
+		    System.out.println("Ran port forward command, going ahead with socket...");
+			try {
+				socket = new ServerSocket(port);
+			} catch (IOException e) {
+				System.out.println("Error creating socket, possibly port already in use...");
+				e.printStackTrace();
+			}
+			if(socket!=null){
+				
+				System.out.println("Socket is instantiated, moving to accept connections...");
+				
+				try {
+					socket.setReuseAddress(true);
+				} catch (SocketException e) {
+					System.out.println("Failure in allowing socket to be reused...");
+					e.printStackTrace();
+				}
+				
+				try {
+					socket.accept();
+				} catch (IOException e) {
+					System.out.println("Failure in socket accepting connections...");
+					e.printStackTrace();
+				}
+			}
+		}else{
+			System.out.println("No android devices found...");
+		}
+		
+		if(socket!=null){
+			try {
+				socket.close();
+			} catch (IOException e) {
+				System.out.println("Failed to close socket...");
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("End of server test.");
+	}
+}
---
+++ src/robot/Robot.java
@@ -0,0 +1,84 @@
+
+package robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import phoneConnection.TestServer;
+import subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static OI oi;
+
+    Command autonomousCommand;
+    
+    TestServer server;
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+        System.out.println("STARTED ROBOT SERVER IN ROBOT INIT");
+        server = new TestServer();
+        System.out.println("FINISHED ROBOT SERVER IN ROBOT INIT");
+    }
+	
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+		Timer.delay(.05);
+	}
+
+    public void autonomousInit() {
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null) autonomousCommand.start();
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to 
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) autonomousCommand.cancel();
+    }
+
+    /**
+     * This function is called when the disabled button is hit.
+     * You can use it to reset subsystems before shutting down.
+     */
+    public void disabledInit(){
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+        LiveWindow.run();
+    }
+}
